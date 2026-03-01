49a65b550c3beb96206ad002f08cc312bd774384
csc34716-coder <csc34716@gmail.com>
2026-03-01 18:36:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
-from routes.upload import upload_bp
+from upload import upload_bp
 
 app = Flask(__name__)
 CORS(app)
