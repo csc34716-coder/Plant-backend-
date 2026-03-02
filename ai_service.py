d547fdf15ba4fc679e92c9da10d4fa05aa02401f
csc34716-coder <csc34716@gmail.com>
2026-03-02 23:08:26 +0530

    Update ai_service.py

--- ai_service.py
+++ ai_service.py
@@ -6,19 +6,21 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 def analyze_plant(image_path):
-    try:
-        with open(image_path, "rb") as img:
-            image_bytes = img.read()
+    response = model.generate_content([
+        """Analyze this plant and return JSON:
+        {
+          "status": "healthy/diseased",
+          "disease": "name",
+          "treatment": "solution"
+        }
+        """,
+        {
+            "mime_type": "image/jpeg",
+            "data": image_bytes
+        }
+    ])
 
-        response = model.generate_content([
-            "Tell in one line: healthy or diseased plant.",
-            {
-                "mime_type": "image/jpeg",
-                "data": image_bytes
-            }
-        ])
-
-        return response.text
+    return response.text
 
     except Exception as e:
         return f"ERROR: {str(e)}"
