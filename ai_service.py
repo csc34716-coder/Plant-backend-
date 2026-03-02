53a6a3483d16dbe0733389310726040da20e8d2d
csc34716-coder <csc34716@gmail.com>
2026-03-02 14:24:05 +0530

    Update ai_service.py

--- ai_service.py
+++ ai_service.py
@@ -1,22 +1,21 @@
-
 import requests
 import os
+import base64
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 def analyze_plant(image_path):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 
     with open(image_path, "rb") as img:
         image_data = img.read()
 
-    import base64
     base64_image = base64.b64encode(image_data).decode("utf-8")
 
     data = {
         "contents": [{
             "parts": [
-                {"text": "Is plant healthy or diseased? Give short answer."},
+                {"text": "Is this plant healthy or diseased? Answer in one line."},
                 {
                     "inline_data": {
                         "mime_type": "image/jpeg",
