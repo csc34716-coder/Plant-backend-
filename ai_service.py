1b7fcf5264cca981fea50b6914993813ea04becf
csc34716-coder <csc34716@gmail.com>
2026-03-02 22:12:05 +0530

    Update ai_service.py

--- ai_service.py
+++ ai_service.py
@@ -3,7 +3,7 @@ import os
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("Gemini 2.5 Flash-Lite")
 
 def analyze_plant(image_path):
     try:
