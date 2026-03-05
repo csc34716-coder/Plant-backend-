02935c72775ae9ee35ae0093a71a443ca7da9f4f
csc34716-coder <csc34716@gmail.com>
2026-03-05 22:44:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from config import Config
-from upload import upload_bp
+import cloudinary
+import cloudinary.uploader
+import tempfile
+import os
+from ai_service import analyze_plant
 
 app = Flask(__name__)
-# इसे पुराने CORS लाइन से बदलें
-CORS(app, resources={r"/*": {"origins": "*"}}) 
+CORS(app)
 
-
-
-# Load config
-app.config.from_object(Config)
-
-# Import routes
-
-
-app.register_blueprint(upload_bp)
+# Cloudinary config
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
 
 @app.route("/")
 def home():
-    return {"message": "Backend running 🚀"}
+    return "Plant AI backend running"
 
-import os
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    image = request.files.get("image")
+    user_query = request.form.get("query")
+
+    if not image:
+        return jsonify({"error": "No image uploaded"})
+
+    # save temporary file
+    temp = tempfile.NamedTemporaryFile(delete=False)
+    image.save(temp.name)
+
+    # AI analysis
+    result = analyze_plant(temp.name, user_query)
+
+    # upload to cloudinary
+    upload = cloudinary.uploader.upload(temp.name)
+
+    image_url = upload["secure_url"]
+
+    return jsonify({
+        "status": result.get("status"),
+        "disease": result.get("disease"),
+        "treatment": result.get("treatment"),
+        "image_url": image_url
+    })
 
 
+if __name__ == "__main__":
+    app.run()
