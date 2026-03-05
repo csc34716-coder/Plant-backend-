92581ba87e2813848acf59938dd4fdac4c7bccc9
csc34716-coder <csc34716@gmail.com>
2026-03-06 01:06:00 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -7,11 +7,10 @@ from ai_service import analyze_plant
 
 upload_bp = Blueprint("upload", __name__)
 
-# Cloudinary config
 cloudinary.config(
-    cloud_name=os.getenv("drtcekb5x"),
-    api_key=os.getenv("257257425648316"),
-    api_secret=os.getenv("apieZE8537oBAXVG9WXUS13xDqE")
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
 
 UPLOAD_FOLDER = "uploads"
@@ -25,19 +24,17 @@ def upload_image():
         return jsonify({"success": False, "error": "No image uploaded"}), 400
 
     file = request.files['image']
-    user_query = request.form.get("user_query", "")
+    user_query = request.form.get("query", "")
 
-    # save temporary image
     filename = str(uuid.uuid4()) + ".jpg"
     filepath = os.path.join(UPLOAD_FOLDER, filename)
     file.save(filepath)
 
     try:
-        # upload to cloudinary
+
         upload_result = cloudinary.uploader.upload(filepath)
         image_url = upload_result["secure_url"]
 
-        # analyze plant
         result = analyze_plant(filepath, user_query)
 
         return jsonify({
