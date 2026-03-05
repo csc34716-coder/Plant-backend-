f87a29550b7b88a162e4acf0ed2090960938a7a7
csc34716-coder <csc34716@gmail.com>
2026-03-05 23:45:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from upload import upload_bp
 
-app = Flask(name)
+app = Flask(__name__)
 CORS(app)
 
-#register upload route
-
+# register upload route
 app.register_blueprint(upload_bp)
 
 @app.route("/")
 def home():
-return "Plant AI Backend Running Successfully"
+    return "Plant AI Backend Running Successfully"
 
-if name == "main":
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
