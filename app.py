be1593133b64c623f4754c7e20bc0a2f2a68d2e5
csc34716-coder <csc34716@gmail.com>
2026-03-06 07:00:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from upload import upload_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -12,4 +13,8 @@ app.register_blueprint(upload_bp)
 def home():
     return "Plant AI Backend Running Successfully"
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
