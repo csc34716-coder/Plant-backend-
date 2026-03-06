5a1ca438b59647077e37df1cfce386ab9f97a651
csc34716-coder <csc34716@gmail.com>
2026-03-06 07:29:25 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -8,7 +8,7 @@ from ai_service import analyze_plant
 upload_bp = Blueprint("upload", __name__)
 
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    cloud_name=os.getenv("CLOUDINARY_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
